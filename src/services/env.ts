52c318498029dbcbfe57f93140677880586b8106
kjohn126 <kadenjohnson@gmail.com>
2023-04-08 13:18:52 -0600

    Update env.ts

--- src/services/env.ts
+++ src/services/env.ts
@@ -1,5 +1,5 @@
 // MAKE SURE TO CHANGE ENDPOINT
-let API_URL = '{{BACKEND-HEROKU-URL-HERE}}/api/';
+let API_URL = 'https://git.heroku.com/ktj-byu-590r-backend-php.git/api';
 if (import.meta.env.MODE === 'development') {
     API_URL = 'http://127.0.0.1:8000/api/';
 }
